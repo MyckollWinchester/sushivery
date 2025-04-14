74367847fea789083dfeaaf006464f4026fafee2
Gabriel Apablaza <gabolonso@gmail.com>
2025-04-13 22:50:49 -0400

    Login frontend implemented

--- frontend/app.vue
+++ frontend/app.vue
@@ -5,7 +5,8 @@
     <nav class="p-4 bg-gray-800 text-white flex gap-4">
       <NuxtLink to="/" class="hover:text-gray-400">Inicio</NuxtLink>
       <NuxtLink to="/default" class="hover:text-gray-400">Menú</NuxtLink>
-      <NuxtLink to="/adminuser" class="hover:text-gray-400">Pedidos</NuxtLink>
+      <NuxtLink to="/adminuser" class="hover:text-gray-400">Usuarios</NuxtLink>
+      <NuxtLink to="/login" class="hover:text-gray-400">Login</NuxtLink>
     </nav>
 
     <NuxtPage />
---
+++ frontend/pages/login.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Button } from '@/components/ui/button'
+</script>
+
+<template>
+    <div class="flex flex-col min-h-screen items-center bg-fukusuke text-white" >
+        <div class="text-center">
+            <img src="/images/logo.jpg" alt="Logo" class="rounded-sm w-60 mx-auto my-10" />
+            <h1 class="font-sans text-4xl">Identificate</h1>    
+        </div>
+        <div class="w-full max-w-sm py-4 space-y-4">
+            <div class="grid w-auto max-w-sm items-center gap-1.5 m-4">
+                <Label for="email">Correo Electronico</Label>
+                <Input id="email" type="email" placeholder="Ingrese email" class="bg-white"/>
+            </div>
+            <div class="grid w-auto max-w-sm items-center gap-1.5 m-4">
+                <Label for="password">Contraseña</Label>
+                <Input id="password" type="password" placeholder="Ingrese contraseña" class="bg-white"/>
+            </div>
+            <a href="#" class="text-sm text-white hover:underline m-4">
+                ¿Olvidaste tu contraseña?
+            </a>
+        </div>
+        <div>
+            <Button variant="outline" class="text-black w-40 rounded-full my-8 py-6">Siguiente</Button>
+        </div>
+        <div class="flex flex-col items-center py-4">
+            <h1 class="text-sm">¿No tienes una cuenta?</h1>
+            <Button class="w-40 py-6 rounded-full">Crear una cuenta</Button>
+        </div>
+    </div>
+
+</template>
