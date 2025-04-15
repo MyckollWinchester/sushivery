588df98255a0e56a9020aedbbc6d78ffaf4b9c99
MyckollWinchester <myckoll.winchester@usm.cl>
2025-04-15 19:39:13 -0400

    Added navbarsito pambisito :v

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,19 +1,60 @@
 <script setup lang="ts">
-import '~/assets/css/global.css'
+import { ref, watch, onMounted } from "vue";
+import { useColorMode } from "@vueuse/core";
+import "~/assets/css/global.css";
+
+const isOpen = ref(false);
+const colorMode = useColorMode();
+const themeIcon = ref("🌞.svg");
+
+const toggleTheme = () => {
+  colorMode.value = colorMode.value === "dark" ? "light" : "dark";
+};
+
+const updateIcon = () => {
+  themeIcon.value = colorMode.value === "dark" ? "🌞.svg" : "🌙.svg";
+};
+
+onMounted(() => {
+  updateIcon();
+});
+watch(colorMode, updateIcon);
 </script>
 
 <template>
   <div>
-    <nav class="p-4 bg-gray-800 text-white flex gap-4">
-      <NuxtLink to="/" class="hover:text-gray-400">Inicio</NuxtLink>
-      <NuxtLink to="/default" class="hover:text-gray-400">Menú</NuxtLink>
-      <NuxtLink to="/adminuser" class="hover:text-gray-400">Usuarios</NuxtLink>
-      <NuxtLink to="/login" class="hover:text-gray-400">Login</NuxtLink>
+    <nav class="flex items-center justify-between p-4">
+      <div class="flex items-center gap-2">
+        <img src="/images/logo.png" alt="Logo" class="w-8 h-8" draggable="false">
+        <div class="text-xl" style="font-family: 'Yuji Mai'">福助</div>
+      </div>
+      <div class="flex items-center gap-4">
+        <div class="flex items-center gap-4">
+          <div class="cursor-pointer text-xl hover:text-fukusuke" draggable="false" @click="toggleTheme" @mousedown.prevent>
+            <img :src="`/svg/${themeIcon}`" alt="Theme Icon" class="w-6 h-6">
+          </div>
+          <button class="md:hidden focus:outline-none" aria-label="Toggle Menu" @click="isOpen = !isOpen">
+            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+          </button>
+        </div>
+        <div class="hidden md:flex gap-6">
+          <NuxtLink to="/" class="hover:text-fukusuke">Inicio</NuxtLink>
+          <NuxtLink to="/default" class="hover:text-fukusuke">Menú</NuxtLink>
+          <NuxtLink to="/adminuser" class="hover:text-fukusuke">Usuarios</NuxtLink>
+          <NuxtLink to="/login" class="hover:text-fukusuke">Login</NuxtLink>
+        </div>
+      </div>
     </nav>
 
-    <NuxtPage />
-    
-    <!-- <h1>zero to hero...</h1> -->
-  </div>
+    <div v-if="isOpen" class="flex flex-col items-start gap-4 p-4 md:hidde">
+      <NuxtLink to="/" class="hover:text-fukusuke">Inicio</NuxtLink>
+      <NuxtLink to="/default" class="hover:text-fukusuke">Menú</NuxtLink>
+      <NuxtLink to="/adminuser" class="hover:text-fukusuke">Usuarios</NuxtLink>
+      <NuxtLink to="/login" class="hover:text-fukusuke">Login</NuxtLink>
+    </div>
 
+    <NuxtPage />
+  </div>
 </template>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -8,19 +8,6 @@ import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious
     <div class="relative w-full px-16">
       <Carousel class="relative w-full">
         <CarouselContent>
-          <!-- <CarouselItem
-            v-for="(_, index) in 10"
-            :key="index"
-            class="basis-full sm:basis-1/2 lg:basis-1/3 xl:basis-1/4 2xl:basis-1/5"
-          >
-            <div class="p-1">
-              <Card>
-                <CardContent class="flex aspect-square items-center justify-center p-6">
-                  <span class="text-4xl font-semibold">{{ index + 1 }}</span>
-                </CardContent>
-              </Card>
-            </div>
-          </CarouselItem> -->
           <CarouselItem class="basis-full sm:basis-1/2 lg:basis-1/3 xl:basis-1/4 2xl:basis-1/5">
             <div class="p-2">
               <Card class="border-none shadow-none">
@@ -29,7 +16,7 @@ import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious
                     src="https://placehold.co/256x256"
                     alt="Producto"
                     class="aspect-square w-full object-cover rounded-md"
-                  />
+                  >
                   <div class="p-2">
                     <p class="[font-size:16px] font-bold ">Lorem Ipsum - 60 piezas</p>
                     <p class="[font-size:12px] text-red-600">26.000 CLP</p>
@@ -39,10 +26,18 @@ import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious
             </div>
           </CarouselItem>
           <CarouselItem class="basis-full sm:basis-1/2 lg:basis-1/3 xl:basis-1/4 2xl:basis-1/5">
-            <div class="p-1">
-              <Card>
-                <CardContent class="flex aspect-square items-center justify-center p-6">
-                  <span class="text-4xl font-semibold">2</span>
+            <div class="p-2">
+              <Card class="border-none shadow-none">
+                <CardContent class="flex flex-col p-0">
+                  <img
+                    src="/images/logo.jpg"
+                    alt="Producto"
+                    class="aspect-square w-full object-cover rounded-md"
+                  >
+                  <div class="p-2">
+                    <p class="[font-size:16px] font-bold ">La Versión Global</p>
+                    <p class="[font-size:12px] text-red-600">Se acerca</p>
+                  </div>
                 </CardContent>
               </Card>
             </div>
