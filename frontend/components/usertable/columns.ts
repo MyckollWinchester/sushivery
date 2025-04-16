590f02fc14af80b0546d90cd6d7410fd839c5f34
Gabriel Apablaza <gabolonso@gmail.com>
2025-04-15 22:25:29 -0400

    Added dropdown Menus to table

--- frontend/components/usertable/columns.ts
+++ frontend/components/usertable/columns.ts
@@ -3,6 +3,7 @@ import type { ColumnDef } from '@tanstack/vue-table'
 import type { User } from '@/types/user'
 import { Button } from '@/components/ui/button'
 import { ArrowUpDown, ChevronDown } from 'lucide-vue-next'
+import DropdownAction from '@/components/usertable/data-table-dropdown.vue'
 
 export const columns: ColumnDef<User>[] = [
     {
@@ -33,6 +34,17 @@ export const columns: ColumnDef<User>[] = [
             onClick: () => column.toggleSorting(column.getIsSorted() === 'asc'),
           }, () => ['Comuna', h(ArrowUpDown, { class: 'ml-2 h-4 w-4' })])
         },
-        cell: ({ row }) => h('div', { class: 'lowercase' }, row.getValue('comuna')),
+        cell: ({ row }) => h('div', { class: '' }, row.getValue('comuna')),
+    },
+    {
+      id: 'actions',
+      enableHiding: false,
+      cell: ({ row }) => {
+        const payment = row.original
+  
+        return h('div', { class: 'relative' }, h(DropdownAction, {
+          payment,
+        }))
+      },
     }
 ]
---
+++ frontend/components/usertable/data-table-dropdown.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+import { MoreHorizontal } from 'lucide-vue-next'
+
+defineProps<{
+  payment: {
+    id: string
+  }
+}>()
+
+function copy(id: string) {
+  navigator.clipboard.writeText(id)
+}
+</script>
+
+<template>
+  <DropdownMenu>
+    <DropdownMenuTrigger as-child>
+      <Button variant="ghost" class="w-8 h-8 p-0">
+        <span class="sr-only">Open menu</span>
+        <MoreHorizontal class="w-4 h-4" />
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent align="end">
+      <DropdownMenuLabel>Opciones</DropdownMenuLabel>
+      <DropdownMenuItem @click="copy(payment.id)">
+        Copiar Rut
+      </DropdownMenuItem>
+      <DropdownMenuItem>Ver Perfil</DropdownMenuItem>
+      <DropdownMenuItem>Editar Usuario</DropdownMenuItem>
+      <DropdownMenuSeparator />
+      <DropdownMenuItem class="bg-fukusuke text-white">Eliminar Usuario</DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
+</template>
